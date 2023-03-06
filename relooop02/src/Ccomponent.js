6b4343e8157e951415355064cb4e782a3cb7b889
seniorHuguenot <seniorhuguenot@gmail.com>
2023-03-06 05:16:33 +0300

    start news reloop02

---
+++ relooop02/src/Ccomponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Menu from "./Menu";
+export default class Ccomponent extends Component {
+  render() {
+    return (
+      <div>
+        <Menu />
+        <h1>Class component</h1>
+      </div>
+    );
+  }
+}
